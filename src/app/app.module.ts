4c253834acce1079b801bf162949b24044a24d27
abdul qayyum <qayyumgb96@gmail.com>
2024-02-13 14:24:44 +0500

    evivornment and packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
-import { NgModule } from '@angular/core';
+import {APP_INITIALIZER, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import {AngularFireModule} from '@angular/fire/compat';
+import {AngularFirestoreModule} from '@angular/fire/compat/firestore';
+import {AngularFireStorageModule} from '@angular/fire/compat/storage';
+import {AngularFireAuth, AngularFireAuthModule} from '@angular/fire/compat/auth';
+import {environment} from "../environments/environment.development";
+
+export function initializeApp(afAuth: AngularFireAuth): () => Promise<null> {
+  return () => {
+    return new Promise((resolve) => {
+        return resolve(null);
+    });
+  };
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{
+    provide: APP_INITIALIZER,
+    multi: true,
+    deps: [AngularFireAuth],
+    useFactory: initializeApp
+  },],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  useEmulators: false,
+  firebase: {
+    apiKey: "AIzaSyDSs7_Un6rSqwVUO4ylwjvad2UnUj2lkzM",
+    authDomain: "opustest-6d0c8.firebaseapp.com",
+    databaseURL: "https://opustest-6d0c8-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "opustest-6d0c8",
+    storageBucket: "opustest-6d0c8.appspot.com",
+    messagingSenderId: "1050438520935",
+    appId: "1:1050438520935:web:93fe3af44b09ae7a0184c2",
+    measurementId: "G-XQE805Z7YE"
+  }
+};
